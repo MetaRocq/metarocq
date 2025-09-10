c5d988d87f29e803ba74425abd52476f85ad0756
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2025-09-10 18:15:55 +0200

    Move Clause stuff to HornClauses

--- common/theories/LoopChecking/HornClauses.v
+++ common/theories/LoopChecking/HornClauses.v
@@ -5,10 +5,215 @@ From Stdlib Require Import Orders OrderedTypeAlt OrderedTypeEx MSetList MSetInte
 From MetaRocq.Utils Require Import utils.
 
 From MetaRocq.Common Require Universes.
+From MetaRocq.Common Require Import Interfaces.
 From Equations Require Import Equations.
 Set Equations Transparent.
 
 Ltac rw l := rewrite_strat (topdown l).
 Ltac rw_in l H := rewrite_strat (topdown l) in H.
 
+Module Clauses (LS : LevelSets).
+  Module Export FLS := FromLevelSets LS.
 
+  Notation univ := nonEmptyLevelExprSet.
+  Definition clause : Type := univ × LevelExpr.t.
+
+  Module Clause.
+
+  Definition t := clause.
+
+  Definition eq : t -> t -> Prop := eq.
+
+  Definition eq_equiv : RelationClasses.Equivalence eq := _.
+
+  Inductive lt_ : t -> t -> Prop :=
+  | lt_clause1 l e e' : LevelExpr.lt e e' -> lt_ (l, e) (l, e')
+  | lt_clause2 l l' b b' : LevelExprSet.lt l.(t_set) l'.(t_set) -> lt_ (l, b) (l', b').
+
+  Definition lt := lt_.
+
+  Global Instance lt_strorder : RelationClasses.StrictOrder lt.
+  Proof.
+    constructor.
+    - intros x X; inversion X; subst. now eapply LevelExpr.lt_strorder in H1.
+      eapply LevelExprSet.lt_strorder; eassumption.
+    - intros x y z X1 X2; invs X1; invs X2; constructor; tea.
+      etransitivity; tea.
+      etransitivity; tea.
+  Qed.
+
+  Definition lt_compat : Proper (Logic.eq ==> Logic.eq ==> iff) lt.
+    intros x x' H1 y y' H2. unfold lt. subst. reflexivity.
+  Qed.
+
+  Definition compare (x y : t) : comparison :=
+    match x, y with
+    | (l1, b1), (l2, b2) =>
+      match LevelExprSet.compare l1.(t_set) l2.(t_set) with
+      | Eq => LevelExpr.compare b1 b2
+      | x => x
+      end
+    end.
+
+  Definition compare_spec :
+    forall x y : t, CompareSpec (x = y) (lt x y) (lt y x) (compare x y).
+  Proof.
+    intros [? ?] [? ?]; cbn; repeat constructor.
+    destruct (LevelExprSet.compare_spec n n0); repeat constructor; tas.
+    eapply LevelExprSet.eq_leibniz in H. apply NonEmptySetFacts.eq_univ in H.
+    subst. cbn in *.
+    destruct (LevelExpr.compare_spec t0 t1); repeat constructor; tas. now subst.
+  Qed.
+
+  #[program] Global Instance reflect_eq_Z : ReflectEq Z := {
+    eqb := Z.eqb
+  }.
+  Next Obligation.
+    destruct (Z.eqb_spec x y); constructor => //.
+  Qed.
+
+  Global Instance reflect_t : ReflectEq t := reflect_prod _ _ .
+
+  Definition eq_dec : forall (l1 l2 : t), {l1 = l2} + {l1 <> l2} := Classes.eq_dec.
+
+  Definition eq_leibniz (x y : t) : eq x y -> x = y := id.
+
+  End Clause.
+
+  Module Clauses := MSetAVL.Make Clause.
+  Module ClausesFact := WFactsOn Clause Clauses.
+  Module ClausesProp := WPropertiesOn Clause Clauses.
+  Module ClausesDecide := WDecide (Clauses).
+  Ltac clsets := ClausesDecide.fsetdec.
+
+  Definition clauses := Clauses.t.
+
+  Lemma filter_add {p x s} : Clauses.Equal (Clauses.filter p (Clauses.add x s)) (if p x then Clauses.add x (Clauses.filter p s) else Clauses.filter p s).
+  Proof.
+    intros i.
+    rewrite Clauses.filter_spec.
+    destruct (eqb_spec i x); subst;
+    destruct (p x) eqn:px; rewrite !Clauses.add_spec !Clauses.filter_spec; intuition auto || congruence.
+  Qed.
+
+  Local Instance proper_fold_transpose {A} (f : Clauses.elt -> A -> A) :
+    transpose eq f ->
+    Proper (Clauses.Equal ==> eq ==> eq) (Clauses.fold f).
+  Proof.
+    intros hf s s' Hss' x ? <-.
+    eapply ClausesProp.fold_equal; tc; tea.
+  Qed.
+  Existing Class transpose.
+
+  Lemma clauses_fold_filter {A} (f : Clauses.elt -> A -> A) (p : Clauses.elt -> bool) cls acc :
+    transpose Logic.eq f ->
+    Clauses.fold f (Clauses.filter p cls) acc =
+    Clauses.fold (fun elt acc => if p elt then f elt acc else acc) cls acc.
+  Proof.
+    intros hf.
+    symmetry. eapply ClausesProp.fold_rec_bis.
+    - intros s s' a eq. intros ->.
+      eapply ClausesProp.fold_equal; tc. auto.
+      intros x.
+      rewrite !Clauses.filter_spec.
+      now rewrite eq.
+    - now cbn.
+    - intros.
+      rewrite H1.
+      rewrite filter_add.
+      destruct (p x) eqn:px => //.
+      rewrite ClausesProp.fold_add //.
+      rewrite Clauses.filter_spec. intuition auto.
+  Qed.
+
+  Definition strict_subset (s s' : LevelSet.t) :=
+    LevelSet.Subset s s' /\ ~ LevelSet.Equal s s'.
+
+  Lemma strict_subset_incl (x y z : LevelSet.t) : LevelSet.Subset x y -> strict_subset y z -> strict_subset x z.
+  Proof.
+    intros hs []. split => //. lsets.
+    intros heq. apply H0. lsets.
+  Qed.
+
+  Lemma strict_subset_cardinal s s' : strict_subset s s' -> LevelSet.cardinal s < LevelSet.cardinal s'.
+  Proof.
+    intros [].
+    assert (LevelSet.cardinal s <> LevelSet.cardinal s').
+    { intros heq. apply H0.
+      intros x. split; intros. now apply H.
+      destruct (LevelSet.mem x s) eqn:hin.
+      eapply LevelSet.mem_spec in hin.
+      auto. eapply LevelSetProp.FM.not_mem_iff in hin.
+      exfalso.
+      eapply LevelSetProp.subset_cardinal_lt in hin; tea.
+      lia. }
+    enough (LevelSet.cardinal s <= LevelSet.cardinal s') by lia.
+    now eapply LevelSetProp.subset_cardinal.
+  Qed.
+
+  Definition premise (cl : clause) := fst cl.
+  Definition concl (cl : clause) := snd cl.
+  Extraction Inline premise concl.
+
+  Definition clause_levels cl :=
+    LevelSet.union (levels (premise cl)) (LevelSet.singleton (level (concl cl))).
+
+  Definition clauses_levels (cls : clauses) : LevelSet.t :=
+    Clauses.fold (fun cl acc => LevelSet.union (clause_levels cl) acc) cls LevelSet.empty.
+
+  Lemma Clauses_In_elements l s :
+    In l (Clauses.elements s) <-> Clauses.In l s.
+  Proof.
+    rewrite ClausesFact.elements_iff.
+    now rewrite InA_In_eq.
+  Qed.
+
+  Lemma clauses_levels_spec_aux l cls acc :
+    LevelSet.In l (Clauses.fold (fun cl acc => LevelSet.union (clause_levels cl) acc) cls acc) <->
+    (exists cl, Clauses.In cl cls /\ LevelSet.In l (clause_levels cl)) \/ LevelSet.In l acc.
+  Proof.
+    eapply ClausesProp.fold_rec.
+    - intros.
+      intuition auto. destruct H1 as [k [hin hl]]. clsets.
+    - intros x a s' s'' hin nin hadd ih.
+      rewrite LevelSet.union_spec.
+      split.
+      * intros [hin'|].
+        left. exists x. split => //.
+        apply hadd. now left.
+        apply ih in H.
+        intuition auto.
+        left. destruct H0 as [k Hk]. exists k. intuition auto. apply hadd. now right.
+      * intros [[k [ins'' ?]]|inacc].
+        eapply hadd in ins''. destruct ins''; subst.
+        + now left.
+        + right. apply ih. now left; exists k.
+        + right. intuition auto.
+  Qed.
+
+  Lemma clauses_levels_spec l cls :
+    LevelSet.In l (clauses_levels cls) <->
+    exists cl, Clauses.In cl cls /\ LevelSet.In l (clause_levels cl).
+  Proof.
+    unfold clauses_levels.
+    rewrite clauses_levels_spec_aux.
+    intuition auto. lsets.
+  Qed.
+
+  Instance clauses_levels_proper : Proper (Clauses.Equal ==> LevelSet.Equal) clauses_levels.
+  Proof.
+    intros cl cl' eq x.
+    rewrite !clauses_levels_spec.
+    now setoid_rewrite eq.
+  Qed.
+
+  Lemma clause_levels_spec l cl :
+    LevelSet.In l (clause_levels cl) <->
+    LevelSet.In l (levels (premise cl)) \/ l = level (concl cl).
+  Proof.
+    unfold clause_levels.
+    now rewrite LevelSet.union_spec LevelSet.singleton_spec.
+  Qed.
+
+  Definition clause_conclusion cl := level (concl cl).
+End Clauses.
--- common/theories/LoopChecking/PartialLoopChecking.v
+++ common/theories/LoopChecking/PartialLoopChecking.v
@@ -7,7 +7,7 @@ From MetaRocq.Utils Require Import utils.
 From MetaRocq.Common Require Universes.
 From Equations Require Import Equations.
 
-From MetaRocq.Common.LoopChecking Require Import Common Interfaces.
+From MetaRocq.Common.LoopChecking Require Import Common Interfaces HornClauses.
 
 Set Equations Transparent.
 
@@ -41,209 +41,9 @@ Module Type LoopCheckingItf (LS : LevelSets).
 End LoopCheckingItf.
 
 Module LoopCheckingImpl (LS : LevelSets).
-  Module Import FLS := FromLevelSets(LS).
+  Module Import Clauses := Clauses(LS).
 
-Notation univ := nonEmptyLevelExprSet.
-Definition clause : Type := univ × LevelExpr.t.
-
-Module Clause.
-  Definition t := clause.
-
-  Definition eq : t -> t -> Prop := eq.
-
-  Definition eq_equiv : RelationClasses.Equivalence eq := _.
-
-  Inductive lt_ : t -> t -> Prop :=
-  | lt_clause1 l e e' : LevelExpr.lt e e' -> lt_ (l, e) (l, e')
-  | lt_clause2 l l' b b' : LevelExprSet.lt l.(t_set) l'.(t_set) -> lt_ (l, b) (l', b').
-
-  Definition lt := lt_.
-
-  Global Instance lt_strorder : RelationClasses.StrictOrder lt.
-  Proof.
-    constructor.
-    - intros x X; inversion X; subst. now eapply LevelExpr.lt_strorder in H1.
-      eapply LevelExprSet.lt_strorder; eassumption.
-    - intros x y z X1 X2; invs X1; invs X2; constructor; tea.
-      etransitivity; tea.
-      etransitivity; tea.
-  Qed.
-
-  Definition lt_compat : Proper (Logic.eq ==> Logic.eq ==> iff) lt.
-    intros x x' H1 y y' H2. unfold lt. subst. reflexivity.
-  Qed.
-
-  Definition compare (x y : t) : comparison :=
-    match x, y with
-    | (l1, b1), (l2, b2) =>
-      match LevelExprSet.compare l1.(t_set) l2.(t_set) with
-      | Eq => LevelExpr.compare b1 b2
-      | x => x
-      end
-    end.
-
-  Definition compare_spec :
-    forall x y : t, CompareSpec (x = y) (lt x y) (lt y x) (compare x y).
-  Proof.
-    intros [? ?] [? ?]; cbn; repeat constructor.
-    destruct (LevelExprSet.compare_spec n n0); repeat constructor; tas.
-    eapply LevelExprSet.eq_leibniz in H. apply NonEmptySetFacts.eq_univ in H.
-    subst. cbn in *.
-    destruct (LevelExpr.compare_spec t0 t1); repeat constructor; tas. now subst.
-  Qed.
-
-  #[program] Global Instance reflect_eq_Z : ReflectEq Z := {
-    eqb := Z.eqb
-  }.
-  Next Obligation.
-    destruct (Z.eqb_spec x y); constructor => //.
-  Qed.
-
-  Global Instance reflect_t : ReflectEq t := reflect_prod _ _ .
-
-  Definition eq_dec : forall (l1 l2 : t), {l1 = l2} + {l1 <> l2} := Classes.eq_dec.
-
-  Definition eq_leibniz (x y : t) : eq x y -> x = y := id.
-End Clause.
-
-Module Clauses := MSetAVL.Make Clause.
-Module ClausesFact := WFactsOn Clause Clauses.
-Module ClausesProp := WPropertiesOn Clause Clauses.
-Module ClausesDecide := WDecide (Clauses).
-Ltac clsets := ClausesDecide.fsetdec.
-
-Definition clauses := Clauses.t.
-
-Lemma filter_add {p x s} : Clauses.Equal (Clauses.filter p (Clauses.add x s)) (if p x then Clauses.add x (Clauses.filter p s) else Clauses.filter p s).
-Proof.
-  intros i.
-  rewrite Clauses.filter_spec.
-  destruct (eqb_spec i x); subst;
-  destruct (p x) eqn:px; rewrite !Clauses.add_spec !Clauses.filter_spec; intuition auto || congruence.
-Qed.
-
-Local Instance proper_fold_transpose {A} (f : Clauses.elt -> A -> A) :
-  transpose eq f ->
-  Proper (Clauses.Equal ==> eq ==> eq) (Clauses.fold f).
-Proof.
-  intros hf s s' Hss' x ? <-.
-  eapply ClausesProp.fold_equal; tc; tea.
-Qed.
-Existing Class transpose.
-
-Lemma clauses_fold_filter {A} (f : Clauses.elt -> A -> A) (p : Clauses.elt -> bool) cls acc :
-  transpose Logic.eq f ->
-  Clauses.fold f (Clauses.filter p cls) acc =
-  Clauses.fold (fun elt acc => if p elt then f elt acc else acc) cls acc.
-Proof.
-  intros hf.
-  symmetry. eapply ClausesProp.fold_rec_bis.
-  - intros s s' a eq. intros ->.
-    eapply ClausesProp.fold_equal; tc. auto.
-    intros x.
-    rewrite !Clauses.filter_spec.
-    now rewrite eq.
-  - now cbn.
-  - intros.
-    rewrite H1.
-    rewrite filter_add.
-    destruct (p x) eqn:px => //.
-    rewrite ClausesProp.fold_add //.
-    rewrite Clauses.filter_spec. intuition auto.
-Qed.
-
-Definition strict_subset (s s' : LevelSet.t) :=
-  LevelSet.Subset s s' /\ ~ LevelSet.Equal s s'.
-
-Lemma strict_subset_incl (x y z : LevelSet.t) : LevelSet.Subset x y -> strict_subset y z -> strict_subset x z.
-Proof.
-  intros hs []. split => //. lsets.
-  intros heq. apply H0. lsets.
-Qed.
-
-Lemma strict_subset_cardinal s s' : strict_subset s s' -> LevelSet.cardinal s < LevelSet.cardinal s'.
-Proof.
-  intros [].
-  assert (LevelSet.cardinal s <> LevelSet.cardinal s').
-  { intros heq. apply H0.
-    intros x. split; intros. now apply H.
-    destruct (LevelSet.mem x s) eqn:hin.
-    eapply LevelSet.mem_spec in hin.
-    auto. eapply LevelSetProp.FM.not_mem_iff in hin.
-    exfalso.
-    eapply LevelSetProp.subset_cardinal_lt in hin; tea.
-    lia. }
-  enough (LevelSet.cardinal s <= LevelSet.cardinal s') by lia.
-  now eapply LevelSetProp.subset_cardinal.
-Qed.
-
-Definition premise (cl : clause) := fst cl.
-Definition concl (cl : clause) := snd cl.
-Extraction Inline premise concl.
-
-Definition clause_levels cl :=
-  LevelSet.union (levels (premise cl)) (LevelSet.singleton (level (concl cl))).
-
-Definition clauses_levels (cls : clauses) : LevelSet.t :=
-  Clauses.fold (fun cl acc => LevelSet.union (clause_levels cl) acc) cls LevelSet.empty.
-
-Lemma Clauses_In_elements l s :
-  In l (Clauses.elements s) <-> Clauses.In l s.
-Proof.
-  rewrite ClausesFact.elements_iff.
-  now rewrite InA_In_eq.
-Qed.
-
-Lemma clauses_levels_spec_aux l cls acc :
-  LevelSet.In l (Clauses.fold (fun cl acc => LevelSet.union (clause_levels cl) acc) cls acc) <->
-  (exists cl, Clauses.In cl cls /\ LevelSet.In l (clause_levels cl)) \/ LevelSet.In l acc.
-Proof.
-  eapply ClausesProp.fold_rec.
-  - intros.
-    intuition auto. destruct H1 as [k [hin hl]]. clsets.
-  - intros x a s' s'' hin nin hadd ih.
-    rewrite LevelSet.union_spec.
-    split.
-    * intros [hin'|].
-      left. exists x. split => //.
-      apply hadd. now left.
-      apply ih in H.
-      intuition auto.
-      left. destruct H0 as [k Hk]. exists k. intuition auto. apply hadd. now right.
-    * intros [[k [ins'' ?]]|inacc].
-      eapply hadd in ins''. destruct ins''; subst.
-      + now left.
-      + right. apply ih. now left; exists k.
-      + right. intuition auto.
-Qed.
-
-Lemma clauses_levels_spec l cls :
-  LevelSet.In l (clauses_levels cls) <->
-  exists cl, Clauses.In cl cls /\ LevelSet.In l (clause_levels cl).
-Proof.
-  unfold clauses_levels.
-  rewrite clauses_levels_spec_aux.
-  intuition auto. lsets.
-Qed.
-
-Instance clauses_levels_proper : Proper (Clauses.Equal ==> LevelSet.Equal) clauses_levels.
-Proof.
-  intros cl cl' eq x.
-  rewrite !clauses_levels_spec.
-  now setoid_rewrite eq.
-Qed.
-
-Lemma clause_levels_spec l cl :
-  LevelSet.In l (clause_levels cl) <->
-  LevelSet.In l (levels (premise cl)) \/ l = level (concl cl).
-Proof.
-  unfold clause_levels.
-  now rewrite LevelSet.union_spec LevelSet.singleton_spec.
-Qed.
-
-Definition clause_conclusion cl := level (concl cl).
-
-Local Open Scope Z_scope.
+  Local Open Scope Z_scope.
 
 Definition model := LevelMap.t (option Z).
 
@@ -5693,7 +5493,7 @@ Proof.
         apply In_add_prems. exists (l, k). split => //. }
       { exists (add_clause n x). rewrite -add_clauses_spec. split => //. apply hadd. now left.
         rewrite clause_levels_spec. right.
-        destruct x; cbn. destruct t => //. }
+        destruct x; cbn. destruct p => //. }
       { intros [cl [hin hl]]; exists cl. split => //.
         move/in_add_clauses: hin => [cl' [incl' ->]].
         apply add_clauses_spec. now apply hadd. }
@@ -6607,7 +6407,7 @@ Lemma valid_model_find {V W cl cls} :
   ~ LevelMap.find (concl cl).1 (model_model v) = None.
 Proof.
   intros v hfind.
-  destruct cl as [prems [concl k]]; unfold LoopCheckingImpl.concl, snd in *; cbn in *.
+  destruct cl as [prems [concl k]]; cbn in *.
   have vmupd := model_of_V v.
   set (pm := premises_model_map _ _) in *.
   move/LevelMapFact.F.not_find_in_iff: hfind; apply.
@@ -7033,7 +6833,7 @@ Lemma check_entails_false {cls cl} :
 Proof.
   funelim (check cls cl) => //.
   set (V := clause_levels cl ∪ clauses_levels cls) in *.
-  destruct cl as [prems [concl k]]; unfold LoopCheckingImpl.concl, snd in *.
+  destruct cl as [prems [concl k]].
   rename val into conclval_v => _. clear H Heq0 Heqcall prf. cbn in he.
   move: (check_atom_value_spec (Some k) conclval_v). rewrite Heq.
   intros r; depelim r. rename H into nent. intros H.
@@ -7671,7 +7471,8 @@ End LoopCheckingImpl.
 
 Module LoopChecking (LS : LevelSets).
   Module Impl := LoopCheckingImpl(LS).
-  Import Impl.FLS.
+  Import Impl.Clauses.
+  Import Impl.Clauses.FLS.
 
   Definition model := Impl.Abstract.t.
 
@@ -7680,17 +7481,17 @@ Module LoopChecking (LS : LevelSets).
   Inductive constraint_type := UnivEq | UnivLe.
   Notation constraint := (univ * constraint_type * univ).
 
-  Definition enforce_constraint (cstr : constraint) (cls : Impl.Clauses.t) : Impl.Clauses.t :=
+  Definition enforce_constraint (cstr : constraint) (cls : Clauses.t) : Clauses.t :=
     let '(l, d, r) := cstr in
     match d with
     | UnivLe =>
-      LevelExprSet.fold (fun lk acc => Impl.Clauses.add (r, lk) acc) (LevelExprSet.t_set l) cls
+      LevelExprSet.fold (fun lk acc => Clauses.add (r, lk) acc) (LevelExprSet.t_set l) cls
     | UnivEq =>
       let cls :=
-        LevelExprSet.fold (fun lk acc => Impl.Clauses.add (r, lk) acc) (LevelExprSet.t_set l) cls
+        LevelExprSet.fold (fun lk acc => Clauses.add (r, lk) acc) (LevelExprSet.t_set l) cls
       in
       let cls' :=
-        LevelExprSet.fold (fun rk acc => Impl.Clauses.add (l, rk) acc) (LevelExprSet.t_set r) cls
+        LevelExprSet.fold (fun rk acc => Clauses.add (l, rk) acc) (LevelExprSet.t_set r) cls
       in cls'
     end.
 
@@ -7702,13 +7503,13 @@ Module LoopChecking (LS : LevelSets).
   (* Returns either a model or a looping universe, i.e. such that u >= u + 1 is implied
      by the constraint *)
   Definition enforce c (m : model) : option (model + univ) :=
-    Impl.Abstract.enforce_clauses m (enforce_constraint c Impl.Clauses.empty).
+    Impl.Abstract.enforce_clauses m (enforce_constraint c Clauses.empty).
 
   (* Returns true is the clause is valid in the model and all its possible consistent extensions.
      Returns false if the constraint results in an inconsistent set of constraints or it simply
      is not valid. *)
   Definition check m c :=
-    Impl.check_clauses m.(Impl.Abstract.clauses) (enforce_constraint c Impl.Clauses.empty).
+    Impl.check_clauses m.(Impl.Abstract.clauses) (enforce_constraint c Clauses.empty).
 
   (* Returns the valuation of the model: a minimal assignement from levels to constraints
     that make the enforced clauses valid. *)
